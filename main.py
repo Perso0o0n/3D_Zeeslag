4a6041b1c543f2613e6b37cb8a0f6152e0728823
Levy <groenkonijn1@gmail.com>
2023-09-25 11:12:04 +0200

    edit lists

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-#main
+width = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"]
+length = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
+invalid_letters = ("klmnopqrstuvwxyz")
+p1_ships = input("Where do you want your ships? ")
+p1_locations = p1_ships.split(", ")
+print(p1_locations)
